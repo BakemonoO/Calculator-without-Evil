693f0d93d9936167bf8b66af1ff422b809016ff5
Bakemono <pervertmel@gmail.com>
2022-11-17 18:05:40 +0600

    add ligth theme for Calculator //

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { createApp } from 'vue'
 import App from './App'
 
 createApp(App).mount('#app')
+
+// \
+// 'number-button': !isNaN(btn.name) || ['.', '<='].includes(btn.name), 'operators': ['/', 'x', '+', '-', 'C', '+/-', '%'].includes(btn.name), 'whiteTheme': !darkTheme}
