7d16feba48314cfe553f38cff8dac0dd5e4ffe5b
Bakemono <pervertmel@gmail.com>
2022-11-19 00:27:22 +0600

    add

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/Calculator-without-Evil/'
+  transpileDependencies: true
 })
